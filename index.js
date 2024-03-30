976799e36ccb85766b2960c66924318e0f46072d
deborah <fabbydebby@gmail.com>
2024-03-30 21:05:46 +0300

    dom modifying lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+const yourName = 'Debbie';
+newHeader.textContent = '${Debbie} is the champion';
+
+const mainElement = document.getElementById('main');
+ if (mainElement) {
+    mainElement.parentNode.replaceChild(newHeader, mainElement);
+ }
